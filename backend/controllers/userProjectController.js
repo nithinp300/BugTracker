d6f4f25b4365471663ac4de788f600ba87c485cc
Nithin Pingili <nithinp300@gmail.com>
2022-08-31 23:42:17 -0500

    created controllers

---
+++ backend/controllers/userProjectController.js
@@ -0,0 +1,86 @@
+const pool = require("../db");
+
+module.exports = {
+  assignUser: async (req, res) => {
+    const { projectId } = req.params;
+    const { userId } = req.body; // If there are multiple users to assign, this will be handled on front end
+    const client = await pool.connect();
+
+    try {
+      await client.query(
+        "INSERT INTO user_projects (project_id, user_id) VALUES ($1, $2)",
+        [projectId, userId]
+      );
+
+      res
+        .status(201)
+        .json({ msg: `User ${userId} assigned to ${projectId} succesfully` });
+    } catch (err) {
+      console.log("assignUsers query error: ", err);
+      res
+        .status(400)
+        .json({ msg: "Please review user project assign creation query" });
+    } finally {
+      await client.release();
+    }
+  },
+  removeAllUsers: async (req, res) => {
+    const { projectId } = req.params;
+    const client = await pool.connect();
+
+    try {
+      await client.query("DELETE FROM user_projects WHERE project_id = $1", [
+        projectId,
+      ]);
+
+      res.status(202).json(`Project users deleted`);
+    } catch (err) {
+      console.log("getProject query error: ", err);
+      res
+        .status(500)
+        .json({ msg: "Unable to remove user_project from database" });
+    } finally {
+      await client.release();
+    }
+  },
+  removeUser: async (req, res) => {
+    const { projectId, userId } = req.params;
+    const client = await pool.connect();
+
+    try {
+      await client.query(
+        "DELETE FROM user_projects WHERE project_id = $1 AND user_id = $2",
+        [projectId, userId]
+      );
+
+      res.status(202).json(`User removed from project`);
+    } catch (err) {
+      console.log("getProject query error: ", err);
+      res
+        .status(500)
+        .json({ msg: "Unable to remove user_project from database" });
+    } finally {
+      await client.release();
+    }
+  },
+  getProjectUsers: async (req, res) => {
+    const { projectId } = req.params;
+    const client = await pool.connect();
+
+    try {
+      const {
+        rows,
+      } = await client.query(
+        "SELECT user_id, first_name, last_name, phone, email FROM users JOIN user_projects ON (user_projects.user_id = users.id) WHERE project_id = $1",
+        [projectId]
+      );
+
+      res.json(rows);
+    } catch (err) {
+      console.log("getProjectUsers query error: ", err);
+      res.status(400).json({ msg: "Please review query" });
+    } finally {
+      client.release();
+    }
+  },
+};
