9cd452e626496f8432000e10dc8224bb4ae5342a
Nithin Pingili <nithinp300@gmail.com>
2022-09-05 23:56:47 -0500

    Spring app

---
+++ backend/bugtrackerapp/src/test/java/com/project/bugtrackerapp/BugtrackerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bugtrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugtrackerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
